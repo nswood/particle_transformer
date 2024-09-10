e8f3280a600e059bb7a0922fe3eabdb0317512b7
Nathaniel Woodward <nswood@holylogin01.rc.fas.harvard.edu>
2024-09-10 11:20:44 -0400

    NeurIPS data processing

--- gen_summary_df.py
+++ gen_summary_df.py
@@ -6,7 +6,6 @@ import argparse
 import numpy as np
 from sklearn.metrics import roc_curve, roc_auc_score, accuracy_score
 
-signals = ['Tbqq', 'Tbl', 'Hqql', 'Wqq', 'Hgg', 'H4q', 'Hbb', 'QCD', 'Hcc', 'Zqq']
 
 # Function to read data from ROOT file and convert to a Pandas DataFrame
 def read_root_file(file_path):
@@ -64,79 +63,136 @@ def bootstrap_rejections(y_true, y_score, target_tprs, samples, sample_size):
     
     return rejection_stats
 
+
+def bootstrap_acc(y_true, y_pred, samples, sample_size):
+    bootstrap_accuracies = []
+    
+    for _ in range(samples):
+        # Randomly select indices for the bootstrap sample
+        indices = np.random.choice(range(len(y_true)), size=sample_size, replace=True)
+        y_true_sample = y_true[indices]
+        y_pred_sample = y_pred[indices]
+                
+        # Calculate accuracy: proportion of correct predictions
+        accuracy = np.mean(y_pred_sample == y_true_sample)
+        bootstrap_accuracies.append(accuracy)
+    
+    # Calculate the mean and standard deviation of the bootstrapped accuracies
+    acc_mean = np.mean(bootstrap_accuracies)
+    acc_std = np.std(bootstrap_accuracies)
+    
+    return acc_mean, acc_std
+
+
 # Main function to process all files and save metrics incrementally
 def process_directory(directory_path, csv_file_path, samples, sample_size):
     first_write = not os.path.exists(csv_file_path)  # Check if the CSV file already exists
-    
+    trained_models = []
+    if not first_write:
+        trained_models = pd.read_csv(csv_file_path)['model_id'].tolist()
+        print(trained_models)
+        
+    if 'h4q' in directory_path:
+        signals = [ 'H4q', 'QCD']
+        PMNN = True
+    elif 'tbqq' in directory_path:
+        signals = ['Tbqq', 'QCD']
+        PMNN = True
+    else:
+        signals = ['Tbqq', 'Tbl', 'Hqql', 'Wqq', 'Hgg', 'H4q', 'Hbb', 'QCD', 'Hcc', 'Zqq']
+        PMNN = False
+        
+        
     # Outer loop: iterate over subdirectories (each representing a model)
     for model_name in os.listdir(directory_path):
-        model_path = os.path.join(directory_path, model_name, 'predict_output')
-        print(f"Processing model: {model_name}")
-        if os.path.isdir(model_path):
-            model_data = pd.DataFrame()  # DataFrame to hold combined data for this model
-            
-            # Inner loop: iterate over .root files in the model's subdirectory
-            for root_file in os.listdir(model_path):
-                if root_file.endswith('.root'):
-                    file_path = os.path.join(model_path, root_file)
-                    df = read_root_file(file_path)  # Read the root file
-                    model_data = pd.concat([model_data, df], axis=0)
-            
-            # Extract score and label columns in the order of signals list
-            score_cols = [f'score_label_{signal}' for signal in signals]
-            label_cols = [f'label_{signal}' for signal in signals]
+        print(f'Loading {model_name}')
+        try:
+            if model_name not in trained_models:
+                print(f"Processing model: {model_name}")
+                model_path = os.path.join(directory_path, model_name, 'predict_output')
+                if os.path.isdir(model_path):
+                    model_data = pd.DataFrame()  # DataFrame to hold combined data for this model
 
-            # Ensure that only the relevant columns exist in model_data
-            score_cols = [col for col in score_cols if col in model_data.columns]
-            label_cols = [col for col in label_cols if col in model_data.columns]
-            
-            # Use DataFrame values to get predictions directly
-            preds = model_data[score_cols].values.tolist()
+                    # Inner loop: iterate over .root files in the model's subdirectory
+                    for root_file in os.listdir(model_path):
+                        if root_file.endswith('.root'):
+                            file_path = os.path.join(model_path, root_file)
+                            df = read_root_file(file_path)  # Read the root file
+                            model_data = pd.concat([model_data, df], axis=0)
 
-            # One-hot encode true labels
-            true_label_indices = model_data[label_cols].values.argmax(axis=1)
-            labels = np.eye(len(signals))[true_label_indices].tolist()
+                    # Extract score and label columns in the order of signals list
+                    score_cols = [f'score_label_{signal}' for signal in signals]
+                    label_cols = [f'label_{signal}' for signal in signals]
+                    # Ensure that only the relevant columns exist in model_data
+                    score_cols = [col for col in score_cols if col in model_data.columns]
+                    label_cols = [col for col in label_cols if col in model_data.columns]
 
-            # Calculate overall accuracy
-            overall_accuracy = accuracy_score(np.argmax(labels, axis=1), np.argmax(preds, axis=1))
-            print(f"Overall accuracy for model {model_name}: {overall_accuracy}")
-            
-            # Initialize a dictionary to hold metrics for each class
-            metrics = {
-                'model_id': model_name,
-                'overall_accuracy': overall_accuracy,
-                'base_name': ''.join(model_name.split('_batch256')[1:])
-            }
+                    # Use DataFrame values to get predictions directly
+                    preds = model_data[score_cols].values.tolist()
+                    # One-hot encode true labels
+                    true_label_indices = model_data[label_cols].values.argmax(axis=1)
+                    labels = np.eye(len(signals))[true_label_indices].tolist()
+                    print(len(labels))
+                    # Calculate overall accuracy
+#                     if PMNN:
+#                         overall_accuracy = accuracy_score(np.argmax(labels, axis=1), 1-np.round(preds))
+#                     else:
+                    overall_accuracy = accuracy_score(np.argmax(labels, axis=1), np.argmax(preds, axis=1))
+                        
+                    print(f"Overall accuracy for model {model_name}: {overall_accuracy}")
 
-            # Calculate metrics for each class
-            for i, signal in enumerate(signals):
-                y_true = np.array([label[i] for label in labels])
-                y_score = np.array([pred[i] for pred in preds])
-                
-                auc, accuracy, rejections, _, _ = calculate_metrics(y_true, y_score)
-                
-                # Bootstrapping for rejection rate uncertainty
-                rejection_stats = bootstrap_rejections(y_true, y_score, [0.3, 0.5, 0.7, 0.99], samples, sample_size)
-                
-                metrics.update({
-                    f'{signal}_auc': auc,
-                    f'{signal}_accuracy': accuracy,
-                    f'{signal}_rejection_30_mean': rejection_stats[0.3][0],
-                    f'{signal}_rejection_30_std': rejection_stats[0.3][1],
-                    f'{signal}_rejection_50_mean': rejection_stats[0.5][0],
-                    f'{signal}_rejection_50_std': rejection_stats[0.5][1],
-                    f'{signal}_rejection_70_mean': rejection_stats[0.7][0],
-                    f'{signal}_rejection_70_std': rejection_stats[0.7][1],
-                    f'{signal}_rejection_99_mean': rejection_stats[0.99][0],
-                    f'{signal}_rejection_99_std': rejection_stats[0.99][1],
-                })
+                    if overall_accuracy < ((1/len(signals))+0.05):
+                        continue
+                    metrics = {
+                        'model_id': model_name,
+                        'overall_accuracy': overall_accuracy,
+                        'base_name': ''.join(model_name.split('_batch1024')[1:])
+                    }
 
-            # Convert the metrics dictionary to a DataFrame
-            results_df = pd.DataFrame([metrics])
+                    if PMNN:
+                        acc_mean, acc_std =  bootstrap_acc(np.argmax(labels, axis=1), np.argmax(preds, axis=1),samples, sample_size)
+                    metrics['acc_mean'] = acc_mean
+                    metrics['acc_std'] = acc_std
+                    
+                    # Initialize a dictionary to hold metrics for each class
+                   
+                    # Calculate metrics for each class
+                    for i, signal in enumerate(signals):
+                        
+                        
+                        y_true = np.array([label[i] for label in labels])
+                        y_score = np.array([pred[i] for pred in preds])
+                        
+                        auc, accuracy, rejections, _, _ = calculate_metrics(y_true, y_score)
 
-            # Save the DataFrame to CSV, appending new results each time
-            results_df.to_csv(csv_file_path, mode='a', index=False, header=first_write)
-            first_write = False  # Only write header for the first model
+                        # Bootstrapping for rejection rate uncertainty
+                        rejection_stats = bootstrap_rejections(y_true, y_score, [0.3, 0.5, 0.7, 0.99], samples, sample_size)
+
+                        metrics.update({
+                            f'{signal}_auc': auc,
+                            f'{signal}_accuracy': accuracy,
+                            f'{signal}_rejection_30_mean': rejection_stats[0.3][0],
+                            f'{signal}_rejection_30_std': rejection_stats[0.3][1],
+                            f'{signal}_rejection_50_mean': rejection_stats[0.5][0],
+                            f'{signal}_rejection_50_std': rejection_stats[0.5][1],
+                            f'{signal}_rejection_70_mean': rejection_stats[0.7][0],
+                            f'{signal}_rejection_70_std': rejection_stats[0.7][1],
+                            f'{signal}_rejection_99_mean': rejection_stats[0.99][0],
+                            f'{signal}_rejection_99_std': rejection_stats[0.99][1],
+                        })
+
+                    # Convert the metrics dictionary to a DataFrame
+                    results_df = pd.DataFrame([metrics])
+
+                    # Save the DataFrame to CSV, appending new results each time
+                    results_df.to_csv(csv_file_path, mode='a', index=False, header=first_write)
+                    trained_models.append(model_name)
+                    first_write = False  # Only write header for the first model
+        except OSError as e:
+            print(f"OSError encountered while reading the file {file_path}: {e}")
+            # Log the error or handle it in a suitable manner
+            continue  # Skip to the next file if an OSError is encountered
+
 
     print(f"All results have been saved incrementally to {csv_file_path}")
 
