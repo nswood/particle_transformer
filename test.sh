6323a8d078883b5eafd6e6029f6045713de1d9c4
Nathaniel Woodward <nswood@holylogin01.rc.fas.harvard.edu>
2024-10-09 16:38:35 -0400

    example testing function

--- test.sh
+++ test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=test
 #SBATCH --partition=gpu_test
-#SBATCH --time=8:00:00
+#SBATCH --time=08:00:00
 
-### e.g. request 4 nodes with 1 gpu each, totally 4 gpus (WORLD_SIZE==4)
+### e.g. request 4 nodes with 1 gpu each, totally 4 gpus (WORLD_SIZE==1)
 ### Note: --gres=gpu:x should equal to ntasks-per-node
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=250G
+#SBATCH --mem=150G
 #SBATCH --chdir=/n/home11/nswood/particle_transformer/
 #SBATCH --output=slurm_monitoring/%x-%j.out
 
@@ -36,45 +36,29 @@ source /n/holystore01/LABS/iaifi_lab/Users/nswood/mambaforge/etc/profile.d/conda
 
 conda activate top_env
 
-# DDP_NGPUS=1 ./train_JetClass.sh PMTrans R 32 R 4 full
-# DDP_NGPUS=1 ./train_JetClass.sh PMTrans R 32 H 4 full
 
-# DDP_NGPUS=1 ./train_JetClass.sh PMTrans R 32 H 8 full
+# DDP_NGPUS=1 ./train_CLR_JetClass.sh PMTrans R 4 R 16 kinpid
 
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 16 R 4
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTransMod R 32 H 4
+# DDP_NGPUS=1 ./train_JetClass.sh PMTrans R 32 R 16 kinpid
 
 
+dimensions=(256 )  # Add your dimensions here
 
+# Loop over each dimension in the list
+for dimension in "${dimensions[@]}"; do
+    half_dimension=$((dimension / 2))  # Calculate half of the dimension
 
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 R 8
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 S 8
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 RxH 4
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 RxS 4
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 HxS 4
-
-
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 R 16
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 H 16
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 S 16
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 RxH 8
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 RxS 8
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 HxS 8
-
-
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 R 32
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 H 32
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 S 32
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 RxH 16
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 RxS 16
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 HxS 16
-
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 R 64
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 H 64
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 S 64
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 RxH 32
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 RxS 32
-# DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R 64 HxS 32
-
-
-
+    # Command with full dimension
+#     DDP_NGPUS=1 ./train_JetClass.sh PMTrans R "$dimension" R 16 kinpid
+#     DDP_NGPUS=1 ./train_TopLandscape.sh PMTrans R "$dimension" R 16 kin --num-epochs 20 --dev-id 'Euclidean_20_epochs'
+#     DDP_NGPUS=1 ./train_JetClass.sh PMTrans H "$dimension" R 16 kinpid
+#     DDP_NGPUS=1 ./train_JetClass.sh PMTrans S "$dimension" R 16 kinpid
+    
+    # Commands with dimensions / 2
+    DDP_NGPUS=1 ./train_JetClass.sh PMTrans RxH "$half_dimension" R 16 kinpid --PM-weight-initialization-factor 1 --inter-man-att 2 --inter-man-att-method 'v2' --dev-id 'tan_space_att_metric' --att-metric 'tan_space' 
+    
+#     DDP_NGPUS=1 ./train_JetClass.sh PMTrans RxS "$half_dimension" R 16 kinpid
+#     DDP_NGPUS=1 ./train_JetClass.sh PMTrans HxS "$half_dimension" R 16 kinpid
+#     DDP_NGPUS=1 ./train_JetClass.sh PMTrans RxR "$half_dimension" R 16 kinpid
+    
+done
